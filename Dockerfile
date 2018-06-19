24717d9556af016df809083f027797717d29c858
Thomas Rzipa <t.rzipa@adrom.at>
2018-06-19 14:20:13 +0200

    initialize project

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM microsoft/aspnetcore:2.0 AS base
+WORKDIR /app
+EXPOSE 80
+
+FROM microsoft/aspnetcore-build:2.0 AS build
+WORKDIR /src
+COPY paprikon.StaticFileServer/paprikon.StaticFileServer.csproj paprikon.StaticFileServer/
+RUN dotnet restore paprikon.StaticFileServer/paprikon.StaticFileServer.csproj
+COPY . .
+WORKDIR /src/paprikon.StaticFileServer
+RUN dotnet build paprikon.StaticFileServer.csproj -c Release -o /app
+
+FROM build AS publish
+RUN dotnet publish paprikon.StaticFileServer.csproj -c Release -o /app
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app .
+ENTRYPOINT ["dotnet", "paprikon.StaticFileServer.dll"]
---
+++ Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace paprikon.StaticFileServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var basepath = Environment.GetEnvironmentVariable("BASEPATH") ?? "";
+
+            app.UseDefaultFiles(basepath);
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    // Requires the following import:
+                    // using Microsoft.AspNetCore.Http;
+                    ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=600");
+                },
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+                RequestPath = basepath,
+            });
+        }
+    }
+}
